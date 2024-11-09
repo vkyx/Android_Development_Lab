f8314844659d7eacf633960658f1dd31dd59d43c
Lemonade <29201741+vkyx@users.noreply.github.com>
2024-11-09 10:10:32 +0800

    Add files via upload

---
+++ Lab7/WorkOut/app/src/main/java/com/example/workout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.workout;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+        startActivity(intent);
+    }
+}
