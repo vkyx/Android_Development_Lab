795841ee1c3d8ab2c6931d9dd519dc1d2700e458
Lemonade <29201741+vkyx@users.noreply.github.com>
2024-11-16 13:24:40 +0800

    Add files via upload

---
+++ Lab9/WorkOut/app/src/main/java/com/example/workout/StopwatchFragment.java
@@ -0,0 +1,107 @@
+package com.example.workout;
+
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Locale;
+
+public class StopwatchFragment extends Fragment implements View.OnClickListener {
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        runTimer(layout);
+        Button startButton = (Button)layout.findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+        Button stopButton = (Button)layout.findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(this);
+        Button resetButton = (Button)layout.findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(this);
+        return layout;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.start_button) {
+            onClickStart();
+        } else if (id == R.id.stop_button) {
+            onClickStop();
+        } else if (id == R.id.reset_button) {
+            onClickReset();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    private void onClickStart() {
+        running = true;
+    }
+
+    private void onClickStop() {
+        running = false;
+    }
+
+    private void onClickReset() {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(View view) {
+        final TextView timeView = (TextView) view.findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs); timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
