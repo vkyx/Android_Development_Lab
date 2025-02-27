b2d1d3746db2e07f6b1e57c94697a9b6514d36de
Lemonade <29201741+vkyx@users.noreply.github.com>
2025-02-27 21:49:05 +0800

    Add files via upload

---
+++ Lab05/Starbuzz/app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,39 @@
+package com.example.starbuzz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                                DrinkActivity.class);
+                        intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+
+        listDrinks.setOnItemClickListener(itemClickListener);
+    }
+}
